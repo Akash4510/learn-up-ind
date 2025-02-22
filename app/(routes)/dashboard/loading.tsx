fc331afe83b27cc5a9be6ae2803b566e0228afd4
Akash Gupta <ag308669@gmail.com>
2025-02-23 01:04:26 +0530

    Add loading page component: implement a loading indicator with spinner and message

---
+++ app/(routes)/dashboard/loading.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Loader2 } from "lucide-react";
+
+const LoadingPage = () => {
+  return (
+    <div className="flex flex-col gap-2 h-full items-center justify-center">
+      <Loader2 className="size-6 animate-spin" />
+      <p className="text-muted-foreground">Loading...</p>
+    </div>
+  );
+};
+
+export default LoadingPage;
