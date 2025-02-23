964457a5ec388061ca4294fb14a2afada67e0f87
Akash Gupta <ag308669@gmail.com>
2025-02-23 23:45:48 +0530

    Add setNewPassword action: implement password reset functionality with validation and token checks

--- actions/auth/index.ts
+++ actions/auth/index.ts
@@ -1,8 +1,17 @@
 import { login } from "./login";
 import { logout } from "./logout";
+import { setNewPassword } from "./new-password";
 import { register } from "./register";
 import { resetPassword } from "./reset-password";
 import { updatePassword } from "./update-password";
 import { verifyEmail } from "./verify-email";
 
-export { login, logout, register, resetPassword, updatePassword, verifyEmail };
+export {
+  login,
+  logout,
+  setNewPassword,
+  register,
+  resetPassword,
+  updatePassword,
+  verifyEmail,
+};
--- app/(routes)/(main)/shipping-policy/page.tsx
+++ app/(routes)/(main)/shipping-policy/page.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-
 import { TitleBlock } from "@/components/title-block";
 
 const RefundPolicyPage = () => {
